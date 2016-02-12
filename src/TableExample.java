1d16627c84499056e4ba255fdc08ce3379af4c4f
Fallah <fallaha@wit.edu>
2016-02-12 11:17:26 -0500

    Skeleton class

--- src/TableExample.java
+++ src/TableExample.java
@@ -1,11 +1,9 @@
 
-
-
-public class AbstractTableModel {
+public class TableExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+
 	}
 
 }
