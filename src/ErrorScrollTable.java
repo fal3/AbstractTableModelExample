d3965ca86f436aec084793aac9eebbf217e4922e
Fallah <fallaha@wit.edu>
2016-02-22 09:47:56 -0500

    Refactored table

--- src/ErrorScrollTable.java
+++ src/ErrorScrollTable.java
@@ -27,51 +27,43 @@ import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
 
-public class RowHeaderTable extends JFrame {
+/**
+ * Called by the DisplayHandlerError. Has a table model of
+ * type ErrorTable. This class is set to visible every time
+ * an error occurs from SC or RC.
+ * 
+ * @author afallah
+ * @created 2-19-16
+ */
+public class ErrorScrollTable extends JFrame {
 
 	public static void main(String[] args) {
-//		RowHeaderTable rht = new RowHeaderTable();
-//		rht.setVisible(true);
-		String strDate = "021816";
-		System.out.println(new Long(strDate));
-	
-		
-	    SimpleDateFormat sdf = new SimpleDateFormat("MMddyy");
-//	    Date date = new Date();
-	    Date str = null;
-		try {
-			str = (Date) sdf.parse(strDate);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-	    System.out.println("formatted date in mm/dd/yy : " + df.format(str));
-
-//		String out = date.fo
+		ErrorScrollTable rht = new ErrorScrollTable();
+		rht.setVisible(true);
 	}
-	ErrorTable et = new ErrorTable();
-	public RowHeaderTable() {
+	private ErrorTable et = new ErrorTable();
+	public ErrorScrollTable() {
 		super("Error Dialog");
-		Dimension prefSize = new Dimension(700, 400);
+		Dimension prefSize = new Dimension(1100, 400);
 		setSize(prefSize);
 		setPreferredSize(prefSize);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+		setDefaultCloseOperation(HIDE_ON_CLOSE);
 		
 		JButton closeButt = new JButton("Close");
 		closeButt.setSize(new Dimension(80, 40));
 		closeButt.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e)
 		    {
-		        System.exit(0);
+		        setVisible(false);
 		    }
 		});
 		
 		DefaultTableColumnModel cm = new DefaultTableColumnModel() {
-			boolean first = true;
+			boolean first = true; 
+			int columnCount = 0;
 			public void addColumn(TableColumn tc) {
-				if(first) {
-					first = false;
+				if(columnCount < 2) {
+					columnCount++;
 					return;
 				}
 				tc.setMinWidth(150);
@@ -81,16 +73,18 @@ public class RowHeaderTable extends JFrame {
 		
 		//Create a column model that will serve as our row header table
 		TableColumnModel rowHeaderModel = new DefaultTableColumnModel() {
-			boolean first = true;
+			int columnCount = 0;
 			public void addColumn(TableColumn tc) {
-				if (first) {
-					tc.setMaxWidth(tc.getPreferredWidth());
+				if (columnCount < 2) {
+					tc.setMaxWidth(75);
 					super.addColumn(tc);
-					first = false;
+					columnCount++;
 				}
 				//drop the rest of the columns, this is the header only
 			}
 		};
+		
+		
 		
 		JTable jt = new JTable(et,cm){
             public boolean getScrollableTracksViewportWidth()
@@ -98,8 +92,9 @@ public class RowHeaderTable extends JFrame {
                 return getPreferredSize().width < getParent().getWidth();
             }
         };
+        
 		
-		//set up header column and hook it to everything
+		//Set up header column and hook it to everything
 		JTable headerColumn = new JTable (et,rowHeaderModel);
 		jt.createDefaultColumnsFromModel();
 		headerColumn.createDefaultColumnsFromModel();
@@ -109,46 +104,23 @@ public class RowHeaderTable extends JFrame {
 		 jt.setCellSelectionEnabled(true);
 		 ListSelectionModel cellSelectionModel = jt.getSelectionModel();
 		 cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-		    cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
-		      public void valueChanged(ListSelectionEvent e) {
-		     
-		        
-		        int[] selectedRow = jt.getSelectedRows();
-		        int[] selectedColumns = jt.getSelectedColumns();
-
-		        for (int i = 0; i < selectedRow.length; i++) {
-		        	String title = (String) jt.getValueAt(selectedRow[i],0);
-		          for (int j = 0; j < selectedColumns.length; j++) {
-					//Store the selected data into a variable
-		            String selectedData = (String) jt.getValueAt(selectedRow[i], selectedColumns[j]);
-		            //Show info on click
-		            System.out.println("Selected: " + selectedData);
-		            JOptionPane.showMessageDialog(rootPane, selectedData, title, NORMAL);
-		            
-		          }
-		        }
-		      }
-	
-
-		    });
 		
-		//make header look pretty
-		headerColumn.setBackground(Color.lightGray);
+		//Make header look pretty
 		headerColumn.setColumnSelectionAllowed(false);
 		headerColumn.setCellSelectionEnabled(false);
+
 		
-		//put in viewport that we can control
+		//Put in viewport that we can control
 		JViewport jv = new JViewport();
 		jv.setView(headerColumn);
 		jv.setPreferredSize(headerColumn.getMaximumSize());
 		
-		//without shutting off autoResizemode our tables wont scroll correctly
+		//Without shutting off autoResizemode our tables wont scroll correctly
 		jt.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		jt.getTableHeader().setReorderingAllowed(false);
 		setResizable(false);
-		
-		
-		//we have too manually attach the row header but the scroll pane will keep them in sync
+			
+		//We have too manually attach the row header but the scroll pane will keep them in sync
 		JScrollPane jsp = new JScrollPane(jt);
 		jsp.setRowHeader(jv);
 		jsp.setCorner(ScrollPaneConstants.UPPER_LEFT_CORNER, headerColumn.getTableHeader());
@@ -158,7 +130,7 @@ public class RowHeaderTable extends JFrame {
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.ipady = 270;      //make this component tall
-		c.ipadx = 670;
+		c.ipadx = 1070;
 		c.weightx = 0.0;
 		c.gridwidth = 4;
 		c.gridx = 0;
@@ -175,11 +147,13 @@ public class RowHeaderTable extends JFrame {
 		addRowButt.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e)
 		    {
-		    	et.makeRowList("635435323423","Some new error description", "The date","The time");
+		    	et.makeRowList("021916","15:10:51:163", "Scan Manager: there is a mismatch in the number of data elements "
+		    			+ "for Powerlink Calibration of phase/frequency initial "
+		    			+ "gains or gain limits." );
 		    }
 		});
 		GridBagConstraints e = new GridBagConstraints();
-		d.gridx = 4;
+		d.gridx = 3;
 		d.gridy = 3;
 		rootPane.add(addRowButt, e);
 	}
--- src/ErrorTable.java
+++ src/ErrorTable.java
@@ -1,25 +1,26 @@
-import java.awt.List;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
+import java.util.LinkedList;
 
-import javax.swing.event.TableModelListener;
 import javax.swing.table.AbstractTableModel;
 
+/**
+ * ErrorTable class is a model class used in RowHeaderTable. 
+ * It's of type AbstractTableModel. It parses values of incoming error 
+ * dialogs and inserts them into this model.
+ * 
+ * @author afallah
+ * @created 2-19-16
+ */
 public class ErrorTable extends AbstractTableModel {
-	int errorCode;
-    String errorDescription;
-    String dateStamp;
-    String timeStamp;
-    
-	ArrayList dataRow;
-	ArrayList data = new ArrayList();
-	String headers[] = {"Row #","Error Code","Error Description" , "Date Stamp","Time Stamp"};
-	 
+	
+	private ArrayList<String> dataRow;
+	private LinkedList data = new LinkedList();
+	private String headers[] = {"Date Stamp","Time Stamp","Error Description"};
+	private static final int MAX_ROW_SIZE = 10;
 	@Override
 	public int getColumnCount() {
 		return headers.length;
@@ -27,55 +28,69 @@ public class ErrorTable extends AbstractTableModel {
 
 	@Override
 	public int getRowCount() {
-		if (this.data.size() >= 10) {
-			return 10;
+		if (this.data.size() >= MAX_ROW_SIZE) {
+			this.data.removeLast();
 		}
 		return this.data.size();
 	}
 
 	@Override
 	public Object getValueAt(int row, int col) {
-		if (col == 0) {
-			return row+1;
-		}
-		ArrayList rowData = new ArrayList();
-		rowData = (ArrayList)data.get(row);
+//		if (col == 0) {
+//			return row+1;
+//		}
+		ArrayList rowData = new ArrayList<String>();
+		rowData = (ArrayList<String>)data.get(row);
 		return rowData.get(col);
 	}
 	
+	@Override
 	public String getColumnName(int col) {
 		return headers[col];
 	}
 	
-	
-	public void addRow()
+	/**
+	 * Called by inner method makeRowList(). Limits the number of 
+	 * rows to 100.
+	 */
+	private void addRow()
     {
 		this.data.add(0,dataRow);
-		if(this.data.size() >= 10) {
+		if(this.data.size() >= MAX_ROW_SIZE) {
 			System.out.println("row count" + getRowCount());
-			System.out.println("Size of 100");
 			fireTableRowsDeleted(0,	getRowCount());
 		} else {
 			fireTableRowsInserted(this.data.size() - 1, this.data.size() - 1);
 		}
-		
-        
     }
 	
-	public void makeRowList(String errorCode,String errorDescription, String dateStamp, String timeStamp) {
-		this.dataRow = new ArrayList();
-		this.dataRow.add("");
-		this.dataRow.add(errorCode);
-		this.dataRow.add(errorDescription);
-		this.dataRow.add(dateStamp);
-		if (getRowCount() >= 10) {
+	/**
+	 * Invoked whenever an error happens from SC or RC 
+	 * through DisplayHandlers
+	 * 
+	 * @param errorDescription
+	 * @param dateStamp 
+	 * @param timeStamp The time stamp is passed in format HH_mm_ss_SSS.
+	 */
+	public void makeRowList( String dateStamp, String timeStamp,String errorDescription) {
+		this.dataRow = new ArrayList<String>();
+		this.dataRow.add(parseDate(dateStamp));
+		if (getRowCount() >= MAX_ROW_SIZE-1) {
 			this.dataRow.add("ALEX");
 		} else {
-			this.dataRow.add(timeStamp);	
+			this.dataRow.add(parseTime(timeStamp));	
 		}
+		this.dataRow.add(errorDescription);
+		
 		addRow();
 	}
 	
+	/**
+	 * Parses date into MM/dd/yyyy format
+	 * 
+	 * @param strDate
+	 * @return
+	 */
 	private String parseDate(String strDate) {
 	    SimpleDateFormat sdf = new SimpleDateFormat("MMddyy");
 	    Date str = null;
@@ -86,6 +101,24 @@ public class ErrorTable extends AbstractTableModel {
 		}
 		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
 		return df.format(str);
+	}
+	
+	/**
+	 * Parses time into hh:mm:ss format
+	 * 
+	 * @param strTime
+	 * @return
+	 */
+	private String parseTime(String strTime) { 
+		SimpleDateFormat stf = new SimpleDateFormat("HH:mm:ss:SSS");
+	    Date tStr = null;
+		try {
+			tStr = (Date) stf.parse(strTime);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		DateFormat tf = new SimpleDateFormat("hh:mm:ss");
+		return tf.format(tStr);
 	}
 	
 }
